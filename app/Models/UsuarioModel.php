22e1991c8531d1cd1e9684d72f2c1aa0e2d0ea6f
Angel-Nevescanin <angel.nevescanin244321@potros.itson.edu.mx>
2024-12-04 01:00:18 -0700

    UsuarioModel
    Ajustes en la clase dentro del paquete models

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ProductoModel extends Model {
+class UsuarioModel extends Model {
 
-    protected $table = "productos"; // Nombre de la tabla en la base de datos
-    protected $primaryKey = "id";   // Llave primaria de la tabla
+    protected $table = "usuarios"; // Cambia a la tabla adecuada para usuarios
+    protected $primaryKey = "id";  // Llave primaria de la tabla
 
     // Campos que pueden ser insertados o actualizados
-    protected $allowedFields = ["nombre", "descripcion", "precio", "imagen", "usuario_id"];
+    protected $allowedFields = ["nombre", "email", "password", "created_at", "updated_at"];
 
     // Habilitar marcas de tiempo automáticas
     protected $useTimestamps = true;
